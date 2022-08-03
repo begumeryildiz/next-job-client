ae12f24d4753004d99631a66c054de23cf7312f6
Begum Eryildiz <begumeryildiz@gmail.com>
2022-08-04 00:00:01 +0200

    created NavBar.js and HomePage.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
+import HomePage from "./pages/HomePage";
 
 function App() {
     return (
         <div className="App">
-
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
         </div>
     );
 }
