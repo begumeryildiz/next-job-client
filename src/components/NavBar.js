bcf38b40fe9f7b92edfaee6988ee7434796f9326
Begum Eryildiz <begumeryildiz@gmail.com>
2022-08-04 17:56:03 +0200

    updated NavBar with isLoggedIn check

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,34 @@
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context"
 
 function Navbar() {
+
+    const { isLoggedIn, user, logOutUser } = useContext(AuthContext);  
+
     return (
         <nav className="NavBar">
            <NavLink to="/" >Home</NavLink>  | 
            <NavLink to="/jobs" >Jobs</NavLink>  | 
            <NavLink to="/companies" >Companies</NavLink>  | 
            <NavLink to="/candidates" >Candidates</NavLink>  | 
-           <NavLink to="/signup" >Sign Up</NavLink>  | 
-           <NavLink to="/login" >Login</NavLink>
+
+
+           {isLoggedIn && (
+                <>
+                    <NavLink to="/projects">Projects</NavLink> | 
+                    <span> Hi, {user && user.name}</span> 
+                    &nbsp;
+                    <button onClick={logOutUser}>Logout</button>
+                </>
+            )}
+
+            {!isLoggedIn && (
+                <>
+                    <NavLink to="/signup">Sign Up</NavLink> | 
+                    <NavLink to="/login">Login</NavLink>
+                </>
+            )}
         </nav>
     );
 }
