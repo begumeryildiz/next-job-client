435cb2ac1e8c7c19fd412c86aca5f514dd6eceac
Begum Eryildiz <begumeryildiz@gmail.com>
2022-08-06 14:06:27 +0200

    bootstrap to NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,42 +1,55 @@
-import { NavLink } from "react-router-dom";
+//import { NavLink } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context"
+import { Container, Navbar, Nav, Button } from "react-bootstrap";
 
-function Navbar() {
+function NavBar() {
 
     const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
     return (
-        <nav className="NavBar">
-            <NavLink to="/" >Home</NavLink>  |
-            <NavLink to="/jobs" >Jobs</NavLink>  |
-            <NavLink to="/companies" >Companies</NavLink>  |
-            <NavLink to="/candidates" >Candidates</NavLink>  |
+        <Navbar className="navbar fw-bold bg-gradient" fixed="sticky" collapseOnSelect expand="lg" bg="primary" variant="dark">
+            <Container >
+                <Navbar.Brand href="/">NEXT JOB</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link  className="fw-bold" href="/jobs" >Jobs</Nav.Link>
+                        <Nav.Link href="/companies" >Companies</Nav.Link>
+                        <Nav.Link href="/candidates" >Candidates</Nav.Link>
+                    </Nav>
+                    <Nav>
+                    {isLoggedIn && (
+                            <>
+                                {user.userType === "user" && (
+                                    <Nav.Link href="/myprofile" >My Profile</Nav.Link>
+                                )}
+                                {user.userType === "company" && (
+                                    <Nav.Link href="/mycompany" >My Company</Nav.Link>
+                                )}
+                                <div className="text-white my-2 mx-2 px-2">{user && user.name}</div>
+                                <Button variant="light" onClick={logOutUser}>Logout</Button>
+                            </>
+                        )}
+                        
 
-
-
-            {isLoggedIn && (
-                <>
-                {user.userType === "user" && (
-                     <NavLink to="/myprofile" >My Profile</NavLink>
-                )}
-                {user.userType === "company" && (
-                     <NavLink to="/mycompany" >My Company</NavLink>
-                )}
-                    <span> Hi, {user && user.name}</span>
-                    &nbsp;
-                    <button onClick={logOutUser}>Logout</button>
-                </>
-            )}
-
-            {!isLoggedIn && (
-                <>
-                    <NavLink to="/signup">Sign Up</NavLink> |
-                    <NavLink to="/login">Login</NavLink>
-                </>
-            )}
-        </nav>
+                        {!isLoggedIn && (
+                            <>
+                                <Nav.Link href="/signup">Sign Up</Nav.Link> |
+                                <Nav.Link href="/login">Login</Nav.Link>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
-export default Navbar; 
+export default NavBar; 
+
+
+
+      
+     
+    
