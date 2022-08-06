810e7d082f1c287a62cc6c3f7153cec44a083d99
Begum Eryildiz <begumeryildiz@gmail.com>
2022-08-06 14:55:46 +0200

    bootstrap to Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
+
+
 function Footer() {
     return (
-        <nav className="Footer">
-            <p>This is footer</p>
-        </nav>
+        <footer class="footer fixed-bottom mt-auto py-4 bg-primary bg-gradient bg-opacity-75 text-white">
+        <div class="container text-center">
+          <small>Copyright &copy; 2022. All rights reserved. Powered by Begum Eryildiz</small>
+        </div>
+      </footer>
+        
     );
 }
 
 
-export default Footer; 
+export default Footer;
+
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProviderWrapper } from "./context/auth.context";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
