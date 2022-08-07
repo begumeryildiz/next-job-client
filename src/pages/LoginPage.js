c657d442f115e586cf61e9820f25928e3d53867b
Begum Eryildiz <begumeryildiz@gmail.com>
2022-08-07 15:11:29 +0200

    added bootstrap to login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import axios from "axios";
 import { NavLink, useNavigate } from "react-router-dom";
+import { Button, Image, Form } from 'react-bootstrap';
 
 import { AuthContext } from '../context/auth.context';
 
@@ -11,7 +12,7 @@ function LoginPage(props) {
 
     const [errorMessage, setErrorMessage] = useState(undefined);
 
-    const {storeToken, authenticateUser} = useContext(AuthContext);
+    const { storeToken, authenticateUser } = useContext(AuthContext);
 
     const navigate = useNavigate();
 
@@ -39,33 +40,74 @@ function LoginPage(props) {
     };
 
     return (
-        <div className="LoginPage">
-            <h1>Login</h1>
+        <section className="vh-100">
+            <div className="container h-100">
+                <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="col col-xl-10">
+                        <div className="card shadow-lg" id="no-scale">
+                            <div className="row g-0">
+                                <div className="col-md-6 col-lg-5 d-none d-md-block">
+                                    <Image src="/next-job-login.png" alt="login form" className="img-fluid h-100 rounded shadow-lg" />
+                                </div>
+                                <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                                    <div className="card-body p-4 p-lg-5 text-black">
 
-            {errorMessage && <p className="error-message">{errorMessage}</p>}
+                                        <Form onSubmit={handleLoginSubmit}>
 
-            <form onSubmit={handleLoginSubmit}>
-                <label>Username:</label>
-                <input
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                /><br /><br />
+                                            <div className="feature-icon d-flex align-items-center mb-3 pb-1">
 
-                <label>Password:</label>
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                /><br /><br />
-
-                <button type="submit">Login</button>
-            </form>
+                                                <Image className="mx-auto" src="/next-job-logo1.png" width={"100"} />
+                                            </div>
 
 
-            <p>Don't have an account yet?</p>
-            <NavLink to={"/signup"}> Sign Up</NavLink>
-        </div>
+                                            <h5 className="fw-normal mb-3 pb-3 text-primary fw-bold">Sign into your account</h5>
+                                            {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+                                            <div className="form-outline mb-4">
+                                                <label className="form-label">Username</label>
+                                                <input type="text"
+                                                    value={username}
+                                                    onChange={(e) => setUsername(e.target.value)} className="form-control form-control-lg"
+                                                    placeholder="nextjob" required />
+
+
+
+                                            </div>
+
+                                            <div className="form-outline mb-4">
+                                                <label className="form-label">Password</label>
+                                                <input type="password"
+                                                    value={password}
+                                                    onChange={(e) => setPassword(e.target.value)} className="form-control form-control-lg"
+                                                    placeholder="********" required />
+
+
+                                            </div>
+
+
+                                            <div className="pt-1 mb-4">
+                                                <Button className="btn btn-primary bg-gradient btn-lg w-100" type="submit">Login</Button>
+                                            </div>
+                                            <p className="mb-5 pb-lg-2" >Don't have an account?
+                                                <NavLink to={"/login"}> Register here</NavLink></p>
+                                            <div>
+                                                <NavLink to="#!" className="small text-muted">Terms of use.</NavLink>
+                                                <NavLink to="#!" className="small text-muted">Privacy policy</NavLink>
+                                            </div>
+                                        </Form>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+
     )
 }
 
--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -36,84 +36,83 @@ function SignupPage(props) {
     };
 
     return (
-        <section className="vh-100">
-            <div className="container h-100">
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                    <div className="col col-xl-10">
-                        <div className="card shadow-lg" id="no-scale" >
-                            <div className="row g-0">
-                                <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                    <Image src="/next-job-sign-up.png" alt="login form" className="img-fluid h-100 rounded shadow-lg" />
-                                </div>
-                                <div className="col-md-6 col-lg-7 d-flex align-items-center">
-                                    <div className="card-body p-4 p-lg-5 text-black">
+        <div className="container py-5 h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+                <div className="col col-xl-10">
+                    <div className="card shadow-lg" id="no-scale" >
+                        <div className="row g-0">
+                            <div className="col-md-6 col-lg-5 d-none d-md-block">
+                                <Image src="/next-job-sign-up.png" alt="signup form" className="img-fluid h-100 rounded shadow-lg" />
+                            </div>
+                            <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                                <div className="card-body p-4 p-lg-5 text-black">
 
-                                        <Form onSubmit={handleSignupSubmit}>
+                                    <Form onSubmit={handleSignupSubmit}>
 
-                                            <div className="feature-icon d-flex align-items-center mb-3 pb-1">
+                                        <div className="feature-icon d-flex align-items-center mb-3 pb-1">
 
-                                                <Image className="mx-auto" src="/next-job-logo1.png" width={"100"} />
-                                            </div>
-
-
-                                            <h5 className="fw-normal mb-3 pb-3 text-primary fw-bold">Create your account</h5>
-                                            {errorMessage && <p classNameName="error-message">{errorMessage}</p>}
-
-                                            <div className="form-outline mb-4">
-                                                <label className="form-label">Username</label>
-                                                <input type="text"
-                                                    value={username}
-                                                    onChange={(e) => setUsername(e.target.value)} className="form-control form-control-lg"
-                                                    placeholder="nextjob" required />
+                                            <Image className="mx-auto" src="/next-job-logo1.png" width={"100"} />
+                                        </div>
 
 
+                                        <h5 className="fw-normal mb-3 pb-3 text-primary fw-bold">Create your account</h5>
+                                        {errorMessage && <p className="error-message">{errorMessage}</p>}
 
-                                            </div>
-
-                                            <div className="form-outline mb-4">
-                                                <label className="form-label">Email address</label>
-                                                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control form-control-lg"
-                                                    placeholder="job@nextjob.com" required />
-
-
-                                            </div>
-
-                                            <div className="form-outline mb-4">
-                                                <label className="form-label">Password</label>
-                                                <input type="password"
-                                                    value={password}
-                                                    onChange={(e) => setPassword(e.target.value)} className="form-control form-control-lg"
-                                                    placeholder="********" required />
+                                        <div className="form-outline mb-4">
+                                            <label className="form-label">Username</label>
+                                            <input type="text"
+                                                value={username}
+                                                onChange={(e) => setUsername(e.target.value)} className="form-control form-control-lg"
+                                                placeholder="nextjob" required />
 
 
-                                            </div>
-                                            <div className="form-outline my-4">
-                                                <label className="form-label">Please Select User Type</label>
-                                                <select className="form-control form-control-lg form-select" value={userType} onChange={(e) => setUserType(e.target.value)}>
-                                                    <option value="candidate" >Candidate</option>
-                                                    <option value="company">Company</option>
-                                                </select>
 
-                                            </div>
+                                        </div>
 
-                                            <div className="pt-1 mb-4">
-                                                <Button className="btn btn-primary bg-gradient btn-lg w-100" type="submit">Register</Button>
-                                            </div>
-                                            <p className="mb-4 pb-lg-2">Already have an account? Click
-                                                <NavLink to={"/login"}> Login</NavLink> to login.</p>
-                                            <div>
-                                                <NavLink to="#!" className="small text-muted">Terms of use.</NavLink>
-                                                <NavLink to="#!" className="small text-muted">Privacy policy</NavLink>
-                                            </div>
-                                        </Form>
-                                    </div>
+                                        <div className="form-outline mb-4">
+                                            <label className="form-label">Email address</label>
+                                            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control form-control-lg"
+                                                placeholder="job@nextjob.com" required />
+
+
+                                        </div>
+
+                                        <div className="form-outline mb-4">
+                                            <label className="form-label">Password</label>
+                                            <input type="password"
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)} className="form-control form-control-lg"
+                                                placeholder="********" required />
+
+
+                                        </div>
+                                        <div className="form-outline my-4">
+                                            <label className="form-label">Please Select User Type</label>
+                                            <select className="form-control form-control-lg form-select" value={userType} onChange={(e) => setUserType(e.target.value)}>
+                                                <option value="candidate" >Candidate</option>
+                                                <option value="company">Company</option>
+                                            </select>
+
+                                        </div>
+
+                                        <div className="pt-1 mb-4">
+                                            <Button className="btn btn-primary bg-gradient btn-lg w-100" type="submit">Register</Button>
+                                        </div>
+                                        <p className="mb-4 pb-lg-2">Already have an account? Click
+                                            <NavLink to={"/login"}> Login</NavLink> to login.</p>
+                                        <div>
+                                            <NavLink to="#!" className="small text-muted">Terms of use.</NavLink>
+                                            <NavLink to="#!" className="small text-muted">Privacy policy</NavLink>
+                                        </div>
+                                    </Form>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div >
-        </section >
+            </div>
+        </div >
+
     )
 }
 
