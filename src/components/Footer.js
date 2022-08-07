204095bc42629e53a18342fcd3693e36b1c8ce82
Begum Eryildiz <begumeryildiz@gmail.com>
2022-08-07 14:20:26 +0200

    added bootstrap to signup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="footer py-4 bg-primary bg-gradient bg-opacity-75 text-white mt-auto">
+    <footer className="footer py-4 bg-primary bg-gradient bg-opacity-90 text-white mt-auto">
       <div className="container text-center">
         <small>Copyright &copy; 2022. All rights reserved. Powered by Begum Eryildiz</small>
       </div>
