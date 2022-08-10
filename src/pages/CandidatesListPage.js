8bc4c01e355fa322131a164458e317d8d451aa55
Begum Eryildiz <begumeryildiz@gmail.com>
2022-08-10 13:02:20 +0200

    fixed cards in Candidates List

--- src/pages/CandidatesListPage.js
+++ src/pages/CandidatesListPage.js
@@ -48,7 +48,7 @@ function CandidatesListPage() {
       <div className="container mt-5">
         <div className="album my-5 pb-2 px-4 bg-primary bg-opacity-25 shadow-lg">
 
-          <div className="row row row-cols-1 row-cols-sm-1 row-cols-md-2 g-4 mb-5">
+          <div className="row row row-cols-1 row-cols-sm-1 row-cols-md-4 g-4 mb-5">
 
             {candidates.map((candidate) => {
               return (
