109fd5d1fc1cfdd77aefbc209dccee2c13ae0d87
Begum Eryildiz <begumeryildiz@gmail.com>
2022-08-11 14:50:10 +0200

    changed the hompage design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,20 @@
 function Footer() {
   return (
+
+  // <footer class="  bg-transparent bg-gradient bg-opacity-80 text-white mt-auto w-100" style={{position: 'relative', bottom: '0'}}>
+  //   <ul class="nav text-white justify-content-center border-bottom pb-3 mb-3">
+  //     <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Home</a></li>
+  //     <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Features</a></li>
+  //     <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+  //     <li class="nav-item"><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+  //     <li class="nav-item"><a href="#" class="nav-link px-2 text-white">About</a></li>
+  //   </ul>
+  //   <p class="text-center text-white">Copyright &copy; 2022. All rights reserved. Design by Begum Eryildiz</p>
+  // </footer>
+
+
+
+
     <footer className="footer py-4 bg-primary bg-gradient bg-opacity-90 text-white mt-auto w-100" style={{position: 'relative', bottom: '0'}}>
       <div className="container text-center">
         <small>Copyright &copy; 2022. All rights reserved. Powered by Begum Eryildiz</small>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,64 +7,92 @@ function HomePage() {
     <div className="HomePage">
       <header>
         <div className="col">
-          <Image src="/next-job-home-header4.png" className="background-cover img-fluid fluid mx-auto d-block w-100" alt="homepage header" />
+          <Image src="/next-job-home-header8.png" className="background-cover img-fluid fluid mx-auto d-block w-100" alt="homepage header" />
         </div>
       </header>
       <div className='container'>
-        <div className="row my-2 py-5">
+        <div className="row">
           <div className="feature col-md-9 text-center fw-bold bg-gradient p-3">
-            <h2>Connecting The Right People To The Right Businesses</h2>
+            <h2 className="text-primary opacity-75">Connecting The Right People To The Right Businesses</h2>
           </div>
 
-          <div className="feature col-md-3 py-3">
+          <div className="feature col-md-3 py-3 text-start">
             <NavLink to="/signup">
-              <Button className="bg-gradient text-white px-5 py-2 mb-4" >Create an Account</Button>
+              <Button className="btn btn-outline-warning btn-rounded fw-bold bg-white" style={{ borderRadius: "40px", color: 'rgb(190, 104, 36)', border: "solid" }} >Create an Account</Button>
             </NavLink>
           </div>
-          <div className="album my-5 pb-2 px-4">
-            <div className="row row row-cols-1 row-cols-sm-1 row-cols-md-3 g-4 ms-1">
+          <div className="album mt-5 pb-2 px-4">
+            <div className="row row row-cols-1 row-cols-sm-1 row-cols-md-3 g-4">
               <div className="col my-4" >
-                <Card className="border-0" >
+                <Card className="homepagecard mx-2 border-0" >
                   <Card.Body>
                     <Card.Title>
                       <Image src="/next-job-icon6.png" style={{ height: "65px" }} />
-                      <h2>Companies</h2>
+                      <h4>Building Company</h4>
                     </Card.Title>
-                    <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae viverra lectus, ac dapibus metus. Vivamus sodales ante nec enim tincidunt luctus. In mauris ante, suscipit in dignissim dapibus, ornare vel elit.
+                    <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae viverra lectus, ac dapibus metus. Vivamus sodales ante nec enim tincidunt luctus.
                     </Card.Text>
                   </Card.Body>
                 </Card>
               </div>
               <div className="col my-4" >
-                <Card  className="border-0" >
+                <Card className="homepagecard mx-2 border-0" >
                   <Card.Body>
                     <Card.Title>
                       <Image src="/next-job-icon4.png" style={{ height: "65px" }} />
-                      <h2>Jobs</h2>
+                      <h4>Finding Best Job</h4>
                     </Card.Title>
                     <Card.Text>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae viverra lectus, ac dapibus metus. Vivamus sodales ante nec enim tincidunt luctus. In mauris ante, suscipit in dignissim dapibus, ornare vel elit.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae viverra lectus, ac dapibus metus. Vivamus sodales ante nec enim tincidunt luctus.
                     </Card.Text>
                   </Card.Body>
                 </Card>
               </div>
               <div className="col my-4" >
-                <Card className="border-0" >
+                <Card className="homepagecard mx-2 border-0" >
                   <Card.Body>
                     <Card.Title>
                       <Image src="/next-job-icon5.png" style={{ height: "65px" }} />
-                      <h2>Candidates</h2>
+                      <h4>Creating Quality Team</h4>
                     </Card.Title>
                     <Card.Text>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae viverra lectus, ac dapibus metus. Vivamus sodales ante nec enim tincidunt luctus. In mauris ante, suscipit in dignissim dapibus, ornare vel elit. 
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae viverra lectus, ac dapibus metus. Vivamus sodales ante nec enim tincidunt luctus.
                     </Card.Text>
                   </Card.Body>
                 </Card>
               </div>
             </div>
           </div>
+          <div className="row row row-cols-1 row-cols-sm-1 row-cols-md-2 g-4">
+            <div className="col" >
+              <Image src="/next-job-about1.png" className="background-cover img-fluid fluid mx-auto d-block" style={{ height: "400px" }} alt="homepage about1" />
+            </div>
+            <div className="col" >
+              <h1 className='fw-bold mt-5 pt-5 text-primary'>Get your dream job today</h1><br />
+              <h3>Explore all the most exciting <span style={{ color: 'rgb(190, 104, 36)' }}><strong>job roles</strong></span> based on your interest and study major.</h3>
+            </div>
+          </div>
+          <div className="row row row-cols-1 row-cols-sm-1 row-cols-md-2 g-4">
+            <div className="col" >
+              <h1 className='fw-bold mt-5 pt-5 text-primary'>Post Your Jobs for Specialists</h1><br />
+              <h3>Next Job helps you post your jobs for finding<span style={{ color: 'rgb(190, 104, 36)' }}><strong> your best teammates</strong></span>. Track your candidates all in one place, and take your hiring on the go.</h3>
+            </div>
+            <div className="col" >
+              <Image src="/next-job-about2.png" className="background-cover img-fluid fluid mx-auto d-block" style={{ height: "400px" }} alt="homepage about2" />
+            </div>
+          </div>
+          <div className="row row row-cols-1 row-cols-sm-1 row-cols-md-2 g-4">
+            <div className="col" >
+              <Image src="/next-job-about3.png" className="background-cover img-fluid fluid mx-auto d-block" style={{ height: "400px" }} alt="homepage about1" />
+            </div>
+            <div className="col" >
+              <h1 className='fw-bold mt-5 pt-5 text-primary'>Find The Best for You</h1><br />
+              <h3>Our industry expertise will find you <span style={{ color: 'rgb(190, 104, 36)' }}><strong>the right role, person, or service</strong></span> in the evolving digital world.</h3>
+            </div>
+          </div>
         </div>
       </div>
+      <Image src="/next-job-footer1.png" className="background-cover img-fluid fluid mx-auto d-block" alt="homwpage footer"/>
     </div>
   );
 }
